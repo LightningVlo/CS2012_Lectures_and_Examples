aa67527f53cdd23229f46540785ab7c738583f3d
Keenan <kknaur@gmail.com>
2018-01-24 17:06:21 -0800

    Lecture 01 updated, Triangle.java and TriangleMain.java updated

--- examples/src/lec01/Triangle.java
+++ examples/src/lec01/Triangle.java
@@ -15,16 +15,18 @@ public class Triangle {
 
     //Constructors
     public Triangle() {
-        //this(1, 1, 1);        //Using "this" to call another constructor.
-        this.side1Length = 1;
-        this.side2Length = 1;
-        this.side3Length = 1;
+        this(1, 1, 1);        //Using "this" to call another constructor.
+
+        //The following 3 lines are equivalent to the previous constructor call.
+        // this.side1Length = 1;
+        // this.side2Length = 1;
+        // this.side3Length = 1;
     }
 
     public Triangle(double side1Length, double side2Length, double side3Length) {
-        this.side1Length = side1Length;
-        this.side2Length = side2Length;
-        this.side3Length = side3Length;
+        this.setSide1Length(side1Length);
+        this.setSide2Length(side2Length);
+        this.setSide3Length(side3Length);
     }
 
     //Getters / Accessors
@@ -43,17 +45,35 @@ public class Triangle {
 
     //Setters / Mutators
     public void setSide1Length(double side1Length) {
-        this.side1Length = side1Length;
+
+        if (side1Length < 0) {
+            System.out.println("ERROR: side 1 length cannot be negative.");
+            System.out.println("Side 1 length was not set.");
+        }
+        else {
+            this.side1Length = side1Length;
+        }
     }
 
     public void setSide2Length(double side2Length) {
-        this.side2Length = side2Length;
+        if (side2Length < 0) {
+            System.out.println("ERROR: Side 2 length cannot be negative.");
+            System.out.println("Side 2 length was not set.");
+        }
+        else {
+            this.side2Length = side2Length;
+        }
     }
 
     public void setSide3Length(double side3Length) {
-        this.side3Length = side3Length;
+        if (side3Length < 0) {
+            System.out.println("ERROR: Side 3 length cannot be negative.");
+            System.out.println("Side 3 length was not set.");
+        }
+        else {
+            this.side3Length = side3Length;
+        }
     }
-
 
     //Other Methods
     public double perimeter() {
@@ -72,10 +92,12 @@ public class Triangle {
     }
 
     //toString() method
+    @Override
     public String toString() {
-        return "";
+        String output = "Side 1 Length:\t" + this.side1Length + "\n" +
+                        "Side 2 Length:\t" + this.side2Length + "\n" +
+                        "Side 3 Length:\t" + this.side3Length;
+
+        return output;
     }
-
-
-
 }
--- examples/src/lec01/TriangleMain.java
+++ examples/src/lec01/TriangleMain.java
@@ -1,10 +1,52 @@
 package lec01;
 
+import java.util.Scanner;
+
 public class TriangleMain {
 
     public static void main(String[] args) {
+        //Creating two triangle objects using the constructors
+        Triangle t1 = new Triangle();
+        Triangle t2 = new Triangle(3, 4, 3);
 
+        //Printing the Triangle objects with the toString() method.
+        System.out.println(t1);
+        System.out.println(t2);
+
+        printTriangleMath(t1);
+        System.out.println();
+        printTriangleMath(t2);
+
+        Triangle t3 = triangleWithUserInput();
+        System.out.println(t3);
     }
 
+    /**
+     * This method demonstrates how to pass an object to a method.
+     *
+     * @param t The Triangle object being passed to the method.
+     */
+    public static void printTriangleMath(Triangle t) {
+        System.out.println("Area = " + t.area());
+        System.out.println("Perimeter = " + t.perimeter());
+    }
 
+    /**
+     * This method shows how to create a Triangle object in a method and return that object.  It also shows how to
+     * pass user input values to an object constructor.
+     * @return  A Triangle object created from user input.
+     */
+
+    public static Triangle triangleWithUserInput() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the lengths of the three sides of a triangle: ");
+        double s1 = in.nextDouble();
+        double s2 = in.nextDouble();
+        double s3 = in.nextDouble();
+
+        Triangle userTriangle = new Triangle(s1, s2, s3);
+
+        return userTriangle;
+    }
 }
